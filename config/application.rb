3ef39d343241595489372bab1d7badcafc77f7ba
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2023-04-06 14:29:21 -0300

    Initial project

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+
+module WorkingMinds
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    config.api_only = true
+  end
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,35 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Faker::Config.locale = 'pt-BR'
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+  Shoulda::Matchers.configure do |cfg|
+    cfg.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+
+      with.library :active_record
+      with.library :active_model
+    end
+  end
+end
